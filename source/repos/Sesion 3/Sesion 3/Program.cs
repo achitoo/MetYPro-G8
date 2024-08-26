6f8b13a56e4a420d49c942c846f0c276351cc49b
achitoo <hbaltodano@uamv.edu.ni>
2024-08-26 10:41:20 -0600

    Clase 26/08/2024

---
+++ source/repos/Sesion 3/Sesion 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using Sesion_3.modelos;
+
+namespace Sesion_3
+{
+    internal class Program
+    {
+        /* Calcular la edad de una persona si es mayor o menor de edad */
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            string nombre;
+            DateTime fechanac;
+            Console.WriteLine("Nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Fecha de nacimiento ");
+            fechanac = DateTime.Parse(Console.ReadLine());
+            persona.Nombre = nombre; 
+            persona.FechaNac = fechanac;
+            Console.WriteLine(persona.EvaluarEdad());
+            
+
+        }
+    }
+}
---
+++ source/repos/Sesion 3/Sesion 3/modelos/Persona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion_3.modelos
+{
+    internal class Persona
+    {
+       public Persona() { }
+        public string Nombre { get; set; }
+        public DateTime FechaNac {  get; set; }
+
+        public int CalcularEdad()
+        {
+            int edad = 0;
+            try
+            {
+                edad = DateTime.Now.Year - FechaNac.Year;
+            }catch(Exception ex)
+            {
+                Console.WriteLine($"Ocurrio un error:  {ex.Message}");
+            }
+            
+            return edad;
+  
+        }
+
+        public string EvaluarEdad()
+            {
+            if (CalcularEdad() >= 180) return "Mayor de edad";
+            return "Menor de edad";
+            }
+                
+        
+    }
+}
